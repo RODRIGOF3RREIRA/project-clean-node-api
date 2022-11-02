9daa31525f7273dd023c295dce147692bf953cbc
RODRIGOF3RREIRA <rodrigoferreira2799@gmail.com>
2022-11-02 17:03:07 -0400

    chore: add coverage on gitignore and add more config to jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,10 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   testEnvironment: 'node',
+  clearMocks: true,
+  collectCoverage: true,
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
